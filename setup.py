bd91132c1718b87a1ca6c35c48e7c14ce7954c74
Marco A. Gutierrez <marcogg@marcogg.com>
2024-03-06 07:05:39 +0000

    Imported upstream version '0.6.1' of 'upstream'

---
+++ setup.py
@@ -0,0 +1,26 @@
+from glob import glob
+
+from setuptools import setup
+
+package_name = 'rqt_robot_dashboard'
+setup(
+    name=package_name,
+    version='0.5.8',
+    package_dir={'': 'src'},
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name + '/images', glob('images/*.svg')),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author="Ze'ev Klapow",
+    maintainer='David V. Lu!!',
+    maintainer_email='davidvlu@gmail.com',
+    description=(
+        'rqt_robot_dashboard provides an infrastructure for building robot dashboard plugins in rqt.'
+    ),
+    license='BSD',
+)
